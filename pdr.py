ac6f0b8129ed08b83e829942adce8d6bf45b6254
AL-22 <68079545+AL-22@users.noreply.github.com>
2022-05-19 22:43:18 -0500

    Update pdr.py

--- pdr.py
+++ pdr.py
@@ -1,35 +1,35 @@
-importar numpy como np
-importar matplotlib. pyplot como plt 
+import numpy as np
+import matplotlib.pyplot as plt 
 
-de matplotlib. importar colores  ListadoColormap
+from matplotlib.colors import ListedColormap
 
-""" Función para colorear las regiones de deciosión """
+""" Function to color the deciosion regions """
 
-def plot_decision_regions(X, y, clasificador, resolución = 0.02):
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
 
-	# generador de marcadores de configuración y mapa de color
+	# setup marker generator and color map
 
-	marcadores = ('s', 'x', 'o', '^', 'v')
-	colores = ('rojo', 'azul', 'verde claro', 'gris', 'cian')
-	cmap = ListedColormap(colors[ : len(np. único(y))])
+	markers = ('s', 'x', 'o', '^', 'v')
+	colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+	cmap = ListedColormap(colors[ : len(np.unique(y))])
 
-	# trazar la superficie de decisión
+	# plot the decision surface
 
-	x1_min, x1_max = X[:, 0]. min() - 1, X[:, 0]. max() + 1
-	x2_min, x2_max = X[:, 1]. min() - 1, X[:, 1]. max() + 1
-	xx1, xx2 = np. meshgrid(np. arange(x1_min, x1_max, resolución),
-		np. arange(x2_min, x2_max, resolución))
+	x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+	x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+	xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+		np.arange(x2_min, x2_max, resolution))
 
-	z = clasificador. predecir(np. array([xx1. ravel(), xx2. ravel()]). T)
-	Z = z. remodelación(xx1. forma)
+	z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+	Z = z.reshape(xx1.shape)
 
-	plt. contourf(xx1, xx2, Z, alfa = 0.4, cmap = cmap)
-	plt. xlim(xx1. min(), xx1. máximo())
-	plt. ylim(xx2. min(), xx2. máximo())
+	plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+	plt.xlim(xx1.min(), xx1.max())
+	plt.ylim(xx2.min(), xx2.max())
 
-	# ejemplos de clase de parcela
+	# plot class samples
 
-	para idx, cl en enumerate(np. unique(y)):
-		plt. scatter(x = X[y == cl, 0], y = X[y == cl, 1],
-			alfa = 0,8, c = cmap(idx), marcador = marcadores[idx],
-			etiqueta = cl)
+	for idx, cl in enumerate(np.unique(y)):
+		plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1],
+			alpha = 0.8, c = cmap(idx), marker = markers[idx],
+			label = cl)
